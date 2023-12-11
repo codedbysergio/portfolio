59b32ba3ba73c7ffa2564a984c04350134a71620
codedbysergio <sergio.r.garcia.tx@gmail.com>
2023-12-11 11:19:45 -0600

    index.css created, projects reversed

--- projects.js
+++ projects.js
@@ -10,13 +10,20 @@ function loadFullstack() {
         else fullStackHeader.innerHTML = "Full Stack Projects";
         let fullStackGrid = document.getElementById("fullstack-projects");
         if (fsProjects != null){
-            for (var project in fsProjects) {
+            const fsProjectsReversed = [];
+            
+            //reverse array so newer projects appear earlier
+            for (var item in fsProjects)
+                fsProjectsReversed.unshift(fsProjects[item]);
+
+            
+            for (var project in fsProjectsReversed) {
                 fullStackGrid.innerHTML += `<div class="col">
                 <div class="card h-100">
-                    <img src="${fsProjects[project]["image"]}" class="card-img-top" alt="..." style="height:250px">
+                    <img src="${fsProjectsReversed[project]["image"]}" class="card-img-top" alt="..." style="height:250px">
                     <div class="card-body">
-                    <h5 class="card-title">${fsProjects[project]["title"]}</h5>
-                    <p class="card-text">${fsProjects[project]["description"]}</p>
+                    <h5 class="card-title">${fsProjectsReversed[project]["title"]}</h5>
+                    <p class="card-text">${fsProjectsReversed[project]["description"]}</p>
                     </div>
                     <div class="row align-items-end" style="padding-bottom: 1rem">
                         <div class="col">
@@ -44,21 +51,38 @@ function loadFrontend() {
         frontendHeader.innerHTML = "Frontend Project";
         else frontendHeader.innerHTML = "Frontend Projects";
         let frontendGrid = document.getElementById("frontend-projects");
+
         if (feProjects != null){
-            for (var project in feProjects) {
+
+            const feProjectsReversed = [];
+            
+            //reverse array so newer projects appear earlier
+            for (var item in feProjects)
+                feProjectsReversed.unshift(feProjects[item]);
+
+            //HTML to create each card in the flexbox
+            for (var project in feProjectsReversed) {
                 frontendGrid.innerHTML += `<div class="col">
                 <div class="card h-100">
-                    <img src="${feProjects[project]["image"]}" class="card-img-top" alt="..." style="height:250px">
+                    <img src="${feProjectsReversed[project]["image"]}" class="card-img-top" alt="..." style="height:250px">
                     <div class="card-body">
-                    <h5 class="card-title">${feProjects[project]["title"]}</h5>
-                    <p class="card-text">${feProjects[project]["description"]}</p>
+                    <h5 class="card-title">${feProjectsReversed[project]["title"]}</h5>
+                    <p class="card-text">${feProjectsReversed[project]["description"]}</p>
                     </div>
                     <div class="row align-items-end" style="padding-bottom: 1rem">
                         <div class="col">
-                            <button type="button" class="btn btn-secondary">Use the App</button>
+                            <button 
+                                type="button" 
+                                class="btn btn-secondary"
+                                onclick="window.location='${feProjectsReversed[project]["app-link"]}';"
+                            >Use the App</button>
                         </div>
                         <div class="col">
-                            <button type="button" class="btn btn-secondary">GitHub Page</button>
+                            <button 
+                                type="button" 
+                                class="btn btn-secondary"
+                                onclick="window.location='${feProjectsReversed[project]["github-link"]}';"
+                                >GitHub Page</button>
                         </div>
                     </div>
                 </div>
