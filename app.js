3ef676140e966627117a72ef631cb29215029e2c
codedbysergio <sergio.r.garcia.tx@gmail.com>
2023-12-19 19:54:26 -0600

    added thumbnails folder and files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // Routes for projects
-app.use('/', proxy('https://sergiocreates.com'));
+//app.use('/', proxy('https://sergiocreates.com'));
 app.use('/random-quote-generator', proxy('https://magical-queijadas-8ee93c.netlify.app/'));
 app.use('/markdown-previewer', proxy('https://euphonious-mandazi-1b5b65.netlify.app/'));
 app.use('/drum-machine', proxy('https://whimsical-kitsune-ec8c9e.netlify.app/'));
